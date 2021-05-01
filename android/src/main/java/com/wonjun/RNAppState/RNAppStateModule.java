67f4580e966b3d6cfe561f1e70d477c4c4d9c8ea
yangnibal <icecube9670@gmail.com>
2021-05-01 22:47:27 +0900

    feat: add android folders

---
+++ android/src/main/java/com/wonjun/RNAppState/RNAppStateModule.java
@@ -0,0 +1,7 @@
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+
+public class RNAppStateModule extends ReactContextBaseJavaModule {
+
+    ReactApplicationContext reactContext;
+}
---
+++ android/src/main/java/com/wonjun/RNAppState/RNAppStatePackage.java
@@ -0,0 +1,10 @@
+package com.wonjun.RNAppState;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+
+public class RNAppStatePackage implements ReactPackage {
+    
+}
